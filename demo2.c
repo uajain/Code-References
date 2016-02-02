92e8a6df48b1330b41190883ea6080415d0e25e5
Umang Jain <mailumangjain@gmail.com>
2016-02-02 10:39:43 +0530

    GtkMenu simulated
    Made a GtkMenu from G_MODEL, prints out text on terminal which option was selected

--- demo2.c
+++ demo2.c
@@ -15,14 +15,14 @@ print_hello (GtkWidget *widget,
 static void
 expelliarmus_func ()
 {
-  g_print ("expelliarmus World\n");
+  g_print ("expelliarmus clicked\n");
 }
 
 
 static void
 incendio_func ()
 {
-  g_print ("incendio World\n");
+  g_print ("incendio clicked\n");
 }
 
 
@@ -36,7 +36,8 @@ activate (GtkApplication *app,
   GtkWidget *button_box;
   GSimpleAction *incendio; 
   GSimpleAction *expelliarmus;
-  GMenu *menu;
+  GMenuModel *menu_model;
+  GtkWidget *menu_button;
   GtkBuilder *builder;
 
   window = gtk_application_window_new (app);
@@ -51,8 +52,6 @@ activate (GtkApplication *app,
   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
   gtk_container_add (GTK_CONTAINER (button_box), button);
  
-  menu = g_menu_new (); 
-
   incendio = g_simple_action_new ("incendio",NULL);
   g_signal_connect_swapped (incendio, "activate", G_CALLBACK (incendio_func), NULL);
   g_action_map_add_action (G_ACTION_MAP (app), G_ACTION (incendio));
@@ -62,8 +61,13 @@ activate (GtkApplication *app,
   g_action_map_add_action (G_ACTION_MAP (app), G_ACTION (expelliarmus));
   g_signal_connect_swapped (expelliarmus, "activate", G_CALLBACK (expelliarmus_func), NULL);
 
+  menu_button = gtk_menu_button_new (); 
+
   builder = gtk_builder_new_from_file ("/home/uajain/app.ui");
-  menu = G_MENU (gtk_builder_get_object (builder, "menu"));
+  menu_model = G_MENU_MODEL (gtk_builder_get_object (builder, "menu"));
+  gtk_menu_button_set_menu_model (GTK_MENU_BUTTON (menu_button), menu_model);
+   gtk_container_add (GTK_CONTAINER (button_box), menu_button);
+  g_object_unref (builder);
 
   gtk_widget_show_all (window);
 }
